0f0f4db80a11be131fdf8fcb5e6d78149fdeaa10
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-01-17 17:35:32 +0100

    testing axios

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,61 +2,7 @@
   <main>
     <div class="container">
       <div class="card">
-        <!-- <img src="" alt=""> -->
-        <h1>New jersey</h1>
-        <h3>Bon jovi</h3>
-        <h4>1988</h4>
-      </div>
-    <div class="card">
-        <!-- <img src="" alt=""> -->
-        <h1>New jersey</h1>
-        <h3>Bon jovi</h3>
-        <h4>1988</h4>
-      </div>
-      <div class="card">
-        <!-- <img src="" alt=""> -->
-        <h1>New jersey</h1>
-        <h3>Bon jovi</h3>
-        <h4>1988</h4>
-      </div>
-      <div class="card">
-        <!-- <img src="" alt=""> -->
-        <h1>New jersey</h1>
-        <h3>Bon jovi</h3>
-        <h4>1988</h4>
-      </div>
-      <div class="card">
-        <!-- <img src="" alt=""> -->
-        <h1>New jersey</h1>
-        <h3>Bon jovi</h3>
-        <h4>1988</h4>
-      </div>
-      <div class="card">
-        <!-- <img src="" alt=""> -->
-        <h1>New jersey</h1>
-        <h3>Bon jovi</h3>
-        <h4>1988</h4>
-      </div>
-      <div class="card">
-        <!-- <img src="" alt=""> -->
-        <h1>New jersey</h1>
-        <h3>Bon jovi</h3>
-        <h4>1988</h4>
-      </div>
-      <div class="card">
-        <!-- <img src="" alt=""> -->
-        <h1>New jersey</h1>
-        <h3>Bon jovi</h3>
-        <h4>1988</h4>
-      </div>
-      <div class="card">
-        <!-- <img src="" alt=""> -->
-        <h1>New jersey</h1>
-        <h3>Bon jovi</h3>
-        <h4>1988</h4>
-      </div>
-      <div class="card">
-        <!-- <img src="" alt=""> -->
+        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
         <h1>New jersey</h1>
         <h3>Bon jovi</h3>
         <h4>1988</h4>
@@ -66,8 +12,34 @@
 </template>
 
 <script>
+// installo axios e lo importo per poi prendere i data
+import axios from 'axios';
+// tengo un esempio dell'array che prendo nei data per comodità
+//  "response": [
+//         {
+//             "poster": "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+//             "title": "New Jersey",
+//             "author": "Bon Jovi",
+//             "genre": "Rock",
+//             "year": "1988"
+//         },
+
 export default {
   name: 'Main',
+  data() {
+    return {
+      discs: null,
+    };
+  },
+  mounted() {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 };
 </script>
 
@@ -81,7 +53,8 @@ export default {
       display: flex;
       flex-wrap: wrap;
       .card {
-        flex-basis: calc(100% / 5);
+        flex-basis: calc((100% / 5) - 2em);
+        margin: 1em;
       }
     }
   }
