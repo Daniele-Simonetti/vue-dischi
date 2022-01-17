3673c8918982e362ce8d8832b4674d7f3e5ea04c
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-01-17 16:30:23 +0100

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Header />
     <Main />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
     Header,
     Main,
   },
@@ -30,6 +30,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
   <header>
-    logo
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+        <i class="fab fa-spotify"></i>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
@@ -10,6 +16,14 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+header {
+  background-color: #2E3A46;
+  height: 100px;
+  i {
+    width: 4%;
+    background-color: green;
+  }
+}
 
 </style>
